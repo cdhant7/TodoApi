32834b6e77805d53c64c33247898121dc63585bf
Sidhant Acharya <acharyasid1@gmail.com>
2024-03-13 16:27:29 +0545

    Add project files.

---
+++ TodoApi/BasicAuthHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace TodoApi
+{
+    public class BasicAuthHandler
+    {
+        private readonly RequestDelegate next;
+        private readonly string relm;
+        public BasicAuthHandler(RequestDelegate next, string relm)
+        {
+            this.next = next;
+            this.relm = relm;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (!context.Request.Headers.ContainsKey("Authorization"))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Unauthorized");
+                return;
+            }
+            //Basic userid password
+            var header = context.Request.Headers["Authorization"].ToString();
+            var encodedcreds = header.Substring(6);
+            var creds = Encoding.UTF8.GetString(Convert.FromBase64String(encodedcreds));
+            string[] uidpwd = creds.Split(':');
+            var uid = uidpwd[0];
+            var password = uidpwd[1];
+
+            if (uid != "Sidhant" || password != "password")
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Unauthorized");
+                return;
+            }
+
+            await next(context);
+
+        }
+
+    }
+}
---
+++ TodoApi/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi;
+using TodoApi.Models;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseMiddleware<BasicAuthHandler>("Test");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
